bcf68ff947b77e6876fd259d356c868da186b0b6
escouvois <bastien.escouvois@gmail.com>
2019-05-03 11:40:35 +0200

    algo 1

--- main.js
+++ main.js
@@ -26,31 +26,6 @@ var problems = {
     }
 };
 
-var solve_problem_dumb = function (problem) {
-    var solution = {
-        problem_id: problem.problem_id,
-        username: USERNAME,
-        orders: []
-    };
-
-    var pos = POSITION_ORIGINE;
-
-    while (problem.orders.length > 0) {
-        console.log(problem.orders.length);
-        // On prend la commande la plus proche et on l'ajoute au trajet du livreur
-        var order = findClosestOrder(problem.orders, pos);
-        solution.orders.push(order.order_id);
-
-        // On garde en mémoire la nouvelle position du livreur
-        pos.lat = order.pos_lat;
-        pos.lng = order.pos_lng;
-
-        // On retire la commande qui vient d'être réalisé
-        problem.orders.splice(problem.orders.indexOf(order), 1);
-    }
-    return solution;
-};
-
 var findClosestOrder = function (orders, pos) {
     orders = orders.sort(function (orderA, orderB) {
         return helpers.compute_dist(orderA.pos_lat, orderA.pos_lng, pos.lat, pos.lng) <= helpers.compute_dist(orderB.pos_lat, orderB.pos_lng, pos.lat, pos.lng)
@@ -58,21 +33,11 @@ var findClosestOrder = function (orders, pos) {
     return orders[orders.length - 1];
 }
 
-
-var findClosestOrders = function (orders, pos, numberOrders) {
-    orders = orders.sort(function (orderA, orderB) {
-        return helpers.compute_dist(orderA.pos_lat, orderA.pos_lng, pos.lat, pos.lng) <= helpers.compute_dist(orderB.pos_lat, orderB.pos_lng, pos.lat, pos.lng)
-    });
-    return orders.slice(numberOrders - 1);
-}
-
 var findRatio = function (orders, pos) {
     orders = orders
-        .map(o => o.amount / helpers.compute_dist(o.pos_lat, o.pos_lng, pos.lat, pos.lng))
         .sort(function (o1, o2) {
-            return o1 >= o2
+            return (o1.amount * 0.5) / helpers.compute_dist(o1.pos_lat, o1.pos_lng, pos.lat, pos.lng) >= o2.amount / helpers.compute_dist(o2.pos_lat, o2.pos_lng, pos.lat, pos.lng)
         });
-
     return orders[orders.length - 1];
 }
 
@@ -86,19 +51,23 @@ let solveProblemV1 = (problem) => {
     var pos = POSITION_ORIGINE;
 
 
-
+    var i = 0;
 
 
     while (problem.orders.length > 0) {
-        //console.log(problem.orders.length);
+        console.log(problem.orders.length);
         // On prend la commande la plus proche et on l'ajoute au trajet du livreur
-        //var closestOrders = findClosestOrders(problem.orders, pos, problem.orders.length < 3 ? problem.orders.length : 3);
-        console.log(pos);
-        var order = findRatio(problem.orders, pos);
 
-
+        i = Math.max.apply(Math, problem.orders.map(function (o) { return o.amount; }))
+        if (i > 0) {
+            var order = findRatio(problem.orders, pos);
+        } else {
+            var order = findClosestOrder(problem.orders, pos);
+        }
         solution.orders.push(order.order_id);
-
+        problem.orders.map(o => {
+            if (o.amount >= 0) o.amount--
+        })
 
         // On garde en mémoire la nouvelle position du livreur
         pos.lat = order.pos_lat;
